6d34436d97e6ec106486faf70508113110352e8d
Vaikundhavasan <uneducatedguys07@gmail.com>
2024-02-22 14:21:15 +0530

    new

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,11 +4,11 @@ import NavBar from "./NavBar";
 const Home = () => {
   return (
     <div className="bg-[url('./assets/home2.jpg')] h-screen bg-cover bg-center">
-      <div className=" w-full h-full backdrop-brightness-[.45] text-white text-center text-4xl font-semibold">
+      <div className=" w-full h-full backdrop-brightness-[.45] text-white text-center font-semibold">
         <NavBar />
         <div className="absolute flex justify-center flex-col items-center w-full h-full ">
-          <h1 className="text-9xl font-bold py-3">BEST BBQ!</h1>
-          <p className="text-4xl">Get The Best Tasty Food</p>
+          <h1 className="text-6xl sm:text-9xl font-bold py-3">BEST BBQ!</h1>
+          <p className="text-2xl sm:text-4xl">Get The Best Tasty Food</p>
         </div>
       </div>
     </div>
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -4,12 +4,12 @@ import { IoMdArrowDropdown } from "react-icons/io";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-between items-center px-6 text-2xl ">
-      <div className="flex justify-center items-center gap-3 ">
-        <FaLocationDot className="text-red-500 text-xl cursor-pointer" />
+    <div className="flex justify-between items-center text-sm px-1 sm:px-6 sm:text-2xl ">
+      <div className="flex justify-center items-center gap-1 sm:gap-3 ">
+        <FaLocationDot className="text-red-500 text-sm sm:text-xl cursor-pointer" />
         <h1 className="hover:text-red-500 ">Grill</h1>
       </div>
-      <div className="capitalize flex gap-6 [&>h1]:cursor-pointer [&>h1]:p-5 ">
+      <div className="capitalize flex gap-2 sm:gap-6 [&>h1]:cursor-pointer sm:[&>h1]:p-5 [&>h1]:p-1">
         <h1 className="bg-red-500">Home</h1>
         <h1 className="hover:text-orange-300 flex justify-center items-center gap-2">
           About us <IoMdArrowDropdown />
