137fbf6f47383cfe3ae75da84cc3261fb47d1164
Vaikundhavasan <uneducatedguys07@gmail.com>
2024-02-22 15:42:38 +0530

    new

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,10 +3,10 @@ import NavBar from "./NavBar";
 
 const Home = () => {
   return (
-    <div className="bg-[url('./assets/home2.jpg')] h-screen bg-cover bg-center">
+    <div className="bg-[url('./assets/home2.jpg')] h-auto sm:h-auto bg-cover bg-center ">
       <div className=" w-full h-full backdrop-brightness-[.45] text-white text-center font-semibold">
         <NavBar />
-        <div className="absolute flex justify-center flex-col items-center w-full h-full ">
+        <div className="flex justify-center flex-col items-center w-full h-full py-32 md:py-60">
           <h1 className="text-6xl sm:text-9xl font-bold py-3">BEST BBQ!</h1>
           <p className="text-2xl sm:text-4xl">Get The Best Tasty Food</p>
         </div>
