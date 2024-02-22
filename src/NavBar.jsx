f7cc20505439655c8755a405eb30bd6d2f969602
Vaikundhavasan <uneducatedguys07@gmail.com>
2024-02-22 15:20:56 +0530

    new

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoMdArrowDropdown } from "react-icons/io";
+import { IoMdMenu } from "react-icons/io";
 
 const NavBar = () => {
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
   return (
-    <div className="flex justify-between items-center text-sm px-1 sm:px-6 sm:text-2xl ">
+    <div className="sm:flex justify-between items-center px-1 sm:px-6 text-2xl ">
       <div className="flex justify-center items-center gap-1 sm:gap-3 ">
-        <FaLocationDot className="text-red-500 text-sm sm:text-xl cursor-pointer" />
+        <FaLocationDot className="text-red-500 text-xl cursor-pointer" />
         <h1 className="hover:text-red-500 ">Grill</h1>
       </div>
-      <div className="capitalize flex gap-2 sm:gap-6 [&>h1]:cursor-pointer sm:[&>h1]:p-5 [&>h1]:p-1">
+      <div className="capitalize sm:flex sm:justify-center sm:items-center gap-2 [&>h1]:cursor-pointer sm:[&>h1]:p-5 [&>h1]:p-1 sm:gap-6 [&>h1]:hidden sm:[&>h1]:flex">
+        <IoMdMenu
+          className="sm:hidden cursor-pointer"
+          onClick={() => setIsMenuClicked(!isMenuClicked)}
+        />
+        {isMenuClicked ? (
+          <div className="absolute flex flex-col gap-5 mt-5 w-52 h-screen z-20 backdrop-brightness-50">
+            <h1 className="bg-red-500">Home</h1>
+            <h1 className="hover:text-orange-300 flex justify-center items-center gap-2">
+              About us <IoMdArrowDropdown />
+            </h1>
+            <h1 className="hover:text-orange-300">Services</h1>
+            <h1 className="hover:text-orange-300">Gallery</h1>
+            <h1 className="hover:text-orange-300">Pricing</h1>
+            <h1 className="hover:text-orange-300">Contact</h1>
+          </div>
+        ) : (
+          ""
+        )}
         <h1 className="bg-red-500">Home</h1>
         <h1 className="hover:text-orange-300 flex justify-center items-center gap-2">
           About us <IoMdArrowDropdown />
