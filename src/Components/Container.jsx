54baa035cd9baca92677b9cb19604bde18ab9ff7
Vaikundhavasan <uneducatedguys07@gmail.com>
2024-02-22 14:50:41 +0530

    New

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Container = ({ imgURL, text, para }) => {
   return (
-    <div className="bg-white w-96 h-96">
+    <div className="bg-white w-52 h-80 sm:w-96 sm:h-96">
       <div
-        className={`${imgURL} h-56 bg-center bg-cover flex justify-center items-end`}
+        className={`${imgURL} h-36 sm:h-56 bg-center bg-cover flex justify-center items-end`}
       >
-        <h1 className="backdrop-brightness-[.6] flex justify-center w-full text-white text-2xl py-3 font-semibold">
+        <h1 className="backdrop-brightness-[.6] flex justify-center w-full text-white text-sm sm:text-2xl py-3 font-semibold">
           {text}
         </h1>
       </div>
 
-      <p className="p-5">{para}</p>
+      <p className="p-2 sm:p-5 text-sm">{para}</p>
     </div>
   );
 };
--- src/Dishes.jsx
+++ src/Dishes.jsx
@@ -6,8 +6,8 @@ const Dishes = () => {
   const imgURL2 = "bg-[url('./assets/service2.jpg')] ";
   const imgURL3 = "bg-[url('./assets/service3.jpg')] ";
   return (
-    <div className="h-screen bg-[#eeb484] ">
-      <div className="text-center px-60 [&>h1]:py-5">
+    <div className="h-auto bg-[#eeb484] ">
+      <div className="text-center px-10 sm:px-60 [&>h1]:py-2 sm:[&>h1]:py-5">
         <h1 className="text-3xl font-semibold">SIGNATURE DISHES</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae
@@ -16,7 +16,7 @@ const Dishes = () => {
           nesciunt.
         </p>
       </div>
-      <div className="flex gap-10 justify-center items-center mt-28">
+      <div className="flex flex-wrap gap-4 sm:gap-10 justify-center items-center mt-10 sm:mt-28 pb-20">
         <Container
           imgURL={imgURL1}
           text="Cobb Salad"
